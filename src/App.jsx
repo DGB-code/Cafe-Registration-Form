9ba2942d60b86dd6c1219967118cbc4f83d23f6c
DGB-code <asyrowmaksat123@gmail.com>
2025-10-01 11:22:37 +0000

    Update phone number placeholder format in App component

--- src/App.jsx
+++ src/App.jsx
@@ -127,7 +127,7 @@ function App() {
               name="phone"
               value={formData.phone}
               onChange={handleChange}
-              placeholder="+1 (555) 123-4567"
+              placeholder="+993 61 234567"
               className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-cafe-brown focus:border-transparent"
             />
           </div>
